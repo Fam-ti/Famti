9fbb1c299cf90ed2734ee8f4ae9510154de4f968
Raghu <raghubalbhadran@gmail.com>
2026-09-21 22:37:43 +0530

    [update for buyer po number

--- famti/models/sale.py
+++ famti/models/sale.py
@@ -103,6 +103,28 @@ class SaleOrder(models.Model):
             if sale_order:
                 invoice.buyer_po_number = sale_order.buyer_po_number
 
+            for invoice_line in invoice.invoice_line_ids:
+                sale_lines = invoice_line.sale_line_ids
+
+                if not sale_lines:
+                    continue
+
+                sale_line = sale_lines[0]
+
+                invoice_line.write({
+                    'description': sale_line.description,
+                    'treatment_in': sale_line.treatment_in,
+                    'treatment_out': sale_line.treatment_out,
+                    'thickness_val': sale_line.thickness_val,
+                    'thickness_uom': sale_line.thickness_uom,
+                    'width_val': sale_line.width_val,
+                    'width_uom': sale_line.width_uom,
+                    'core_id': sale_line.core_id,
+                    'length_val': sale_line.length_val,
+                    'length_uom': sale_line.length_uom,
+                    'remarks': sale_line.remarks,
+                })
+
         return invoices
     
     @api.model
